8dd03ec660a5e762cc727294469063faca06c6aa
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-30 14:18:17 -0400

    User Morgan Holbart saved changes to main.py at 03/30/2024, 18:18:16 UTC time.

--- d73bdea2-cd04-47df-826b-c082f31b9482/main.py
+++ d73bdea2-cd04-47df-826b-c082f31b9482/main.py
@@ -22,13 +22,11 @@ class TradingStrategy(Strategy):
         sma_current = SMA(self.ticker, data["ohlcv"], 20)[-1]
         sma_previous = SMA(self.ticker, data["ohlcv"], 20)[-20]
 
-        log(f"SMA delta is " + str(sma_current - previous))
-
         # Check if the current SMA has dropped more than 7% compared to the previous day
         if sma_current < sma_previous * 0.99:
-            log("SMA dropped more than 5%, increasing PHO stake by 10%.")
+            log("SMA dropped more than 10%, increasing PHO stake by 1%.")
             # Increase the stake in PHO by 10% more of the portfolio, constrained to not exceed 100%
-            new_allocation = min(1, current_allocation + 0.1)
+            new_allocation = min(1, current_allocation + 0.01)
         else:
             # If the condition isn't met, maintain the current allocation
             new_allocation = current_allocation
